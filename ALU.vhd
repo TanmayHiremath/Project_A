d9757d95aea430f0827de8b6cd7a8423b41e83be
Akashfokane2507 <63790654+Akashfokane2507@users.noreply.github.com>
2020-12-04 13:54:13 +0530

    .

--- ALU.vhd
+++ ALU.vhd
@@ -38,11 +38,26 @@ port(
 	O     : out bit_vector(0 to 15)
 	);
 end component;
-signal temp1,temp2,temp3,temp4:bit_vector(0 to 15);	
+
+signal temp1,temp2,temp3,temp4:bit_vector(0 to 15);
+signal var1,var2,var3,var4 : bit ;
+signal out_temp : bit_vector(0 to 15);
 		
 begin
 F1:	KS_ADDER port map(I0=>D0,I1=>D1,C_in=>'0',A=>temp1,C_out=>C);
 F2: subtractor port map(I0=>D0,I1=>D1,O=>temp2);
 F3: XOR1 port map(D0=>D0,D1=>D1,A=>temp3);
 F4: NAND1 port map(D0=>D0,D1=>D1,A=>temp4);
+
+var1 <= (not S1) AND (not S0)
+var2 <= (not S1) AND (S0)
+var3 <= (S1) AND (not S0)
+var4 <= (S1) AND (S0)
+
+expression : for i in 0 to 15 generate
+	out_temp(i) <= (var1 and temp1(i)) OR (var2 and temp2(i)) OR (var3 and temp3(i)) OR (var4 and temp4(i))
+end generate expression;
+
+A <= out_temp
+
 end structure;
